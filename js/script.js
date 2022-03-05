32176c3a025c562ac384e7a35f01133015e04749
vernellmiller <nezeey@gmail.com>
2022-03-05 13:49:36 -0500

    adds condition for noName, testing dynamic height

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@
 document.querySelector("button").addEventListener("click", function () {
   let personOneName = document.querySelector(".name").value;
   document.querySelector(".persons").innerHTML = personOneName;
+
+  if (!personOneName) {
+    document.querySelector(".persons").innerHTML = "Person 1";
+  }
+
   console.log(personOneName);
 
   const foot = document.querySelector("#foot").value;
@@ -10,4 +15,37 @@ document.querySelector("button").addEventListener("click", function () {
 
   const inch = document.querySelector("#inch").value;
   console.log(inch);
+
+  document.querySelector("#p1").style.height = 50 + "%";
+
+  //   switch (foot) {
+  //     case 1:
+  //       document.querySelector("#p1").style.height = 10 + "%";
+  //       break;
+  //     case 2:
+  //       document.querySelector("#p1").style.height = 20 + "%";
+  //       break;
+  //     case 3:
+  //       document.querySelector("#p1").style.height = 30 + "%";
+  //       break;
+  //     case 4:
+  //       document.querySelector("#p1").style.height = 50 + "%";
+  //       break;
+  //     case 5:
+  //       document.querySelector("#p1").style.height = 50 + "%";
+  //       break;
+  //     case 6:
+  //       document.querySelector("#p1").style.height = 60 + "%";
+  //       break;
+  //     case 7:
+  //       document.querySelector("#p1").style.height = 70 + "%";
+  //       break;
+  //     case 8:
+  //       document.querySelector("#p1").style.height = 100 + "%";
+  //       break;
+  //   }
 });
+
+// TODO create function that adjusts the height of .persons div according to the foot value.
+
+// TODO create function that adjusts the height of .persons div according to the inch value.
