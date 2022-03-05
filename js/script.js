01f74eacf7f28a368d4818f52efddd00ba55d2d7
vernellmiller <nezeey@gmail.com>
2022-03-05 15:27:22 -0500

    percentage function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 "use strict";
+const percentage = function (target) {
+  const total = 8;
+  const percent = (target / total) * 100;
+  return percent;
+};
 
 document.querySelector("button").addEventListener("click", function () {
   let personOneName = document.querySelector(".name").value;
@@ -18,33 +23,35 @@ document.querySelector("button").addEventListener("click", function () {
 
   document.querySelector("#p1").style.height = 50 + "%";
 
-  //   switch (foot) {
-  //     case 1:
-  //       document.querySelector("#p1").style.height = 10 + "%";
-  //       break;
-  //     case 2:
-  //       document.querySelector("#p1").style.height = 20 + "%";
-  //       break;
-  //     case 3:
-  //       document.querySelector("#p1").style.height = 30 + "%";
-  //       break;
-  //     case 4:
-  //       document.querySelector("#p1").style.height = 50 + "%";
-  //       break;
-  //     case 5:
-  //       document.querySelector("#p1").style.height = 50 + "%";
-  //       break;
-  //     case 6:
-  //       document.querySelector("#p1").style.height = 60 + "%";
-  //       break;
-  //     case 7:
-  //       document.querySelector("#p1").style.height = 70 + "%";
-  //       break;
-  //     case 8:
-  //       document.querySelector("#p1").style.height = 100 + "%";
-  //       break;
-  //   }
+  console.log(percentage(foot));
 });
+
+//   switch (foot) {
+//     case 1:
+//       document.querySelector("#p1").style.height = 10 + "%";
+//       break;
+//     case 2:
+//       document.querySelector("#p1").style.height = 20 + "%";
+//       break;
+//     case 3:
+//       document.querySelector("#p1").style.height = 30 + "%";
+//       break;
+//     case 4:
+//       document.querySelector("#p1").style.height = 50 + "%";
+//       break;
+//     case 5:
+//       document.querySelector("#p1").style.height = 50 + "%";
+//       break;
+//     case 6:
+//       document.querySelector("#p1").style.height = 60 + "%";
+//       break;
+//     case 7:
+//       document.querySelector("#p1").style.height = 70 + "%";
+//       break;
+//     case 8:
+//       document.querySelector("#p1").style.height = 100 + "%";
+//       break;
+//   }
 
 // TODO create function that adjusts the height of .persons div according to the foot value.
 
