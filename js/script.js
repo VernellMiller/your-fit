eb7da89743cf19d913f37ac18bdef482e00395d3
vernellmiller <nezeey@gmail.com>
2022-03-04 21:03:02 -0500

    wip: onclick function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// document.getElementById("p1").innerHTML = prompt("What is your name?");
+// let personOneName = document.querySelector(".name").value;
+// document.querySelector(".persons").innerHTML = personOneName;
+// console.log(personOneName);
+
+document.querySelector("button").addEventListener("onclick", function () {
+  //   let personOneName = document.querySelector(".name").value;
+  //   document.querySelector(".persons").innerHTML = personOneName;
+  //   console.log(personOneName);
+  console.log(document.querySelector(".name").value);
+});
