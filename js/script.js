978e3307826290a86c6ecb996cd3381395f4065c
vernellmiller <nezeey@gmail.com>
2022-03-05 10:30:36 -0500

    captures and logs foot and inch values

--- js/script.js
+++ js/script.js
@@ -4,4 +4,10 @@ document.querySelector("button").addEventListener("click", function () {
   let personOneName = document.querySelector(".name").value;
   document.querySelector(".persons").innerHTML = personOneName;
   console.log(personOneName);
+
+  const foot = document.querySelector("#foot").value;
+  console.log(foot);
+
+  const inch = document.querySelector("#inch").value;
+  console.log(inch);
 });
