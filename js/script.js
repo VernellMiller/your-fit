57cf6979d848819b152bc75a2aa68add7cc50d89
vernellmiller <nezeey@gmail.com>
2022-03-05 10:23:05 -0500

    fix: click function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-// let personOneName = document.querySelector(".name").value;
-// document.querySelector(".persons").innerHTML = personOneName;
-// console.log(personOneName);
+"use strict";
 
-document.querySelector("button").addEventListener("onclick", function () {
-  //   let personOneName = document.querySelector(".name").value;
-  //   document.querySelector(".persons").innerHTML = personOneName;
-  //   console.log(personOneName);
-  console.log(document.querySelector(".name").value);
+document.querySelector("button").addEventListener("click", function () {
+  let personOneName = document.querySelector(".name").value;
+  document.querySelector(".persons").innerHTML = personOneName;
+  console.log(personOneName);
 });
